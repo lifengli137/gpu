758f1fe95beb865c95ce8a0121a02a9c7de01042
v-lifli <v-lifli@microsoft.com>
2020-03-19 21:52:15 +0000

    add GPU reordering tools

---
+++ run_gpu_topo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+nvidia-smi topo -p2p n | grep OK | head -n 8 | awk '{$1=""; print $0}' | sed 's/ //' | tee /tmp/topo
+
+g++ -o gpu_topo gpu_topo.cpp
+order=$(./gpu_topo /tmp/topo)
+
+if [[ $? -eq 0 ]]; then
+	echo $order
+	export CUDA_VISIBLE_DEVICES=$order
+fi
